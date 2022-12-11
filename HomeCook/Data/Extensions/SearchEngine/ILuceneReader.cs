2684b89a89863c7b3d0248dd29f7e5a5c2186f08
Łukasz Harla <lukaszharla@gmail.com>
2022-12-11 21:45:41 +0100

    search engine delete not used services

--- HomeCook/Data/Extensions/SearchEngine/ILuceneReader.cs
+++
@@ -1,9 +0,0 @@
-﻿using HomeCook.DTO.SearchEngine;
-
-namespace HomeCook.Data.Extensions.SearchEngine
-{
-    public interface ILuceneReader
-    {
-        List<LuceneRecipeSearchResultItem> Search(string searchString, RecipeFilters filters, int? fieldIdToSearch = null, IEnumerable<int> documentIdsToSearch = null);
-    }
-}
